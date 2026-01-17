310373bf76b69305608d31e5fe14671350beadcd
scott0127 <scott5497ify@gmail.com>
2026-01-17 19:30:45 +0800

    提示

--- app/pages/map.vue
+++ app/pages/map.vue
@@ -262,11 +262,11 @@
             </div>
             
             <!-- 警告訊息 -->
-            <div v-if="selectedFilters.length > 10" class="mb-2 px-2 py-1 rounded bg-orange-50 text-orange-600 text-xs flex items-center gap-1">
+            <div v-if="selectedFilters.length > 30" class="mb-2 px-2 py-1 rounded bg-orange-50 text-orange-600 text-xs flex items-center gap-1">
               <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3" viewBox="0 0 20 20" fill="currentColor">
                 <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd" />
               </svg>
-              超過 10 種查詢會比較慢喔
+              超過30種查詢會比較慢喔
             </div>
 
             <div class="flex gap-2">
